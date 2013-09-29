381351703855ce453ae5108cb7b63ff2323e68ed
Mattes Groeger <info@mattes-groeger.de>
2013-09-30 00:54:23 +0200

    Simplify curl call

--- lib/index.rb
+++ lib/index.rb
@@ -18,7 +18,7 @@ def update_data
   items = MONGO.collection('items')
 
   # fetch remote data
-  html = `curl 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3474718/view=switch/index.html' -H 'Cookie: Test=Sun, 31 Jan 2100 23:00:00 GMT; ARD-Mediathek=2; xtvrn=$511893$; mobileRedirect=false; Test=Sun, 31 Jan 2100 23:00:00 GMT; ARD-Mediathek=2; ard_mediathek_player_settings=%7B%22lastUsedPlugin%22%3A0%2C%22changedVolumeValue%22%3A0.76%2C%22changedMuteValue%22%3Afalse%2C%22changeSubtitleValue%22%3Afalse%7D' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Host: www.ardmediathek.de' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36' -H 'Accept: */*' -H 'Referer: http://www.ardmediathek.de/fernsehen' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' --compressed`
+  html = `curl 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3474718/view=switch/index.html'`
   entries = ARDMediathekParser.new.parse_html(html)
   puts "Retrieved #{entries.size} entries..."
   
@@ -34,15 +34,11 @@ def update_data
       
       puts "NEW ============================================="
       # get details
-      ARDMediathekParser.new.parse_details(`curl '#{details_url(entry)}'`, entry)
+      ARDMediathekParser.new.parse_details(`curl 'http://www.ardmediathek.de/ard/servlet/ajax-cache/#{entry[:id]}/view=ajax/isFromList=true/index.html'`, entry)
       items.insert(entry)
       puts entry.to_s + "\n\n"
     end
   end
   
   items.find.sort([["discovered", -1]]).limit(25).to_a
-end
-
-def details_url(entry)
-  "http://www.ardmediathek.de/ard/servlet/ajax-cache/#{entry[:id]}/view=ajax/isFromList=true/index.html".strip
 end
