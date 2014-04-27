39c18b459abbb9d4299164bf69d26db3ebc32faa
Mattes Groeger <info@mattes-groeger.de>
2014-04-27 22:33:13 +0200

    Add test for different mediathek stream

--- spec/update_spec.rb
+++ spec/update_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../lib/update.rb'
 
-describe "Update" do
+shared_examples "ard mediathek" do |url, details_url|
 
-  it "should update 1 new ard doku item remotely" do
+  it "should update 1 new item remotely" do
     config = {
-      url: 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3474718/view=switch/index.html',
-      details_url: "http://www.ardmediathek.de/ard/servlet/ajax-cache/{{id}}/view=ajax/isFromList=true/index.html",
+      url: url,
+      details_url: details_url,
       max_details: 1,
       parser:      {file: 'mediathek', class: 'ARDMediathekParser'},
     }
@@ -18,6 +18,21 @@ describe "Update" do
 
     Feed::update(config, fetcher, storage)
   end
+end
+
+describe "fetching from ard mediathek" do
+
+  it_should_behave_like "ard mediathek",
+    "http://www.ardmediathek.de/ard/servlet/ajax-cache/3474718/view=switch/index.html",
+    "http://www.ardmediathek.de/ard/servlet/ajax-cache/{{id}}/view=ajax/isFromList=true/index.html"
+
+  it_should_behave_like "ard mediathek",
+    "http://www.ardmediathek.de/ard/servlet/ajax-cache/4585472/view=switch/index.html",
+    "http://www.ardmediathek.de/ard/servlet/ajax-cache/{{id}}/view=ajax/isFromList=true/index.html"
+
+end
+
+describe "Parsing cached files" do
 
   it "should update 5 new local ard doku items" do
     config = {
